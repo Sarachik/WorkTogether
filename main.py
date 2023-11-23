e91cde8b4914d846931e7579e1440d6b163fe1b9
Sarachik <mirdor78@inbox.ru>
2023-11-23 16:00:04 +0300

    Создал ветки и места для записей

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print('NewpROJECT')
+
+def dimaG():
+    pass
+
+def dimaF():
+    pass
+
+def artem():
+    pass
+
+def kirill():
+    pass
+
+def teacher():
+    pass
+
+def dimab():
+    pass
+
+def nikita():
+    pass
+
+def sergey():
+    pass
